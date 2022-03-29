873380d46b76a48a2bff9acd10da3468b85302c0
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-03-29 22:38:01 +0530

    Create CheckString.js

---
+++ CheckString.js
@@ -0,0 +1,9 @@
+function check_string()
+{
+    let text="m";
+            if(text.length===0){
+            console.log(text.length===0);}
+            else{
+                console.log(text.length===0);}
+}
+check_string();
