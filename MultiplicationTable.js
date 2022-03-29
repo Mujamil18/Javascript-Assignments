fc9222e931aafd30f562ff46459a5938ea5248a5
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-03-29 22:47:25 +0530

    Create MultiplicationTable.js

---
+++ MultiplicationTable.js
@@ -0,0 +1,6 @@
+ var number= prompt("Enter a number to display the multiplication table");
+ var limit=prompt("Enter the limit of multiplication table");
+  for(let i=1; i<=limit ; i++)
+  {
+     console.log(number +" * " + i+" = " + (number * i));
+  }
