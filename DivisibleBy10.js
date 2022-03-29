df69ce3c1d99e1e8a50f3ce81d029cd7a96d5b4f
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-03-29 22:43:47 +0530

    Create DivisibleBy10.js

---
+++ DivisibleBy10.js
@@ -0,0 +1,14 @@
+var number;
+            function return_boolean(number)
+            {
+            number= prompt("Enter a number");
+           if(number%10===0)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+        }
+           console.log(return_boolean(number));
